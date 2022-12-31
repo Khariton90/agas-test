6f8f4f0594b670cf1933ba293d6dd76300fa06cd
Khariton90 <82729640+Khariton90@users.noreply.github.com>
2022-12-31 19:52:51 +0300

    vue deploy files

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/agas-cloud/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/agas-test/'
+    : '/',
   transpileDependencies: true, 
   css: {
     loaderOptions: {
